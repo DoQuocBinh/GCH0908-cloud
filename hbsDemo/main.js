6feff3a51d9af40483806b14495c8a63297687b4
DoQuocBinh <binhdq@fpt.edu.vn>
2022-08-03 11:55:13 +0700

    ok

--- hbsDemo/main.js
+++ hbsDemo/main.js
@@ -2,6 +2,7 @@ var express = require('express')
 const Handlebars = require('handlebars')
 var app = express()
 var hbs = require('hbs')
+var fs= require('fs')
 
 app.set('view engine','hbs')
 app.use(express.urlencoded({extended:true}))
@@ -9,6 +10,10 @@ app.use(express.static('public'))
 
 hbs.registerHelper('checkPriceGT200',(price)=>{
     return price > 200;
+})
+
+hbs.registerHelper('checkColor',(color, color1)=>{
+    return color == color1;
 })
 hbs.registerHelper("mbold", function(options) {
     return new Handlebars.SafeString('<span style="color:blue">' + options.fn(this) + "</span>");
@@ -27,11 +32,21 @@ app.get('/',(req,res)=>{
             name : 'Coffee',
             price: 330,
             onsale : true
-
         }
     ]
+    let content = fs.readFileSync('product.txt',"utf-8").trim()
+    let products3 = []
+    content.split('\n').forEach(element => {
+        let p = {
+            name: element.split(";")[0],
+            color: element.split(";")[1],
+            price: element.split(";")[2],
+        }
+        products3.push(p)
+    });
     //truyen bien name vao trang home
-    res.render('home',{'name':name,'products':products,'products2':products2})
+    res.render('home',{'name':name,'products':products,'products2':products2
+                                        ,'products3':products3})
 })
 
 const PORT = process.env.PORT || 5000
