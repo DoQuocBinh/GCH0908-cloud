4960a62d20fea8347530998e656c0c3b9e7f646e
DoQuocBinh <binhdq@fpt.edu.vn>
2022-08-03 09:13:28 +0700

    ok

---
+++ hbsDemo/main.js
@@ -0,0 +1,39 @@
+var express = require('express')
+const Handlebars = require('handlebars')
+var app = express()
+var hbs = require('hbs')
+
+app.set('view engine','hbs')
+app.use(express.urlencoded({extended:true}))
+app.use(express.static('public'))
+
+hbs.registerHelper('checkPriceGT200',(price)=>{
+    return price > 200;
+})
+hbs.registerHelper("mbold", function(options) {
+    return new Handlebars.SafeString('<span style="color:blue">' + options.fn(this) + "</span>");
+});
+
+app.get('/',(req,res)=>{
+    var name = "Q Q"
+    var products = ["Vovol","Honda","Yamaha"]
+    var products2 = [
+        {
+            name : 'cocacola',
+            price: 120,
+            onsale: false
+        },
+        {
+            name : 'Coffee',
+            price: 330,
+            onsale : true
+
+        }
+    ]
+    //truyen bien name vao trang home
+    res.render('home',{'name':name,'products':products,'products2':products2})
+})
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT)
+console.log('Server is running!')
