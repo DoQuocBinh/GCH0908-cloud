c0946b688980c6d78f7009736204215090cc8871
DoQuocBinh <binhdq@fpt.edu.vn>
2022-08-24 12:01:49 +0700

    ok

--- sessionDemo/app.js
+++ sessionDemo/app.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var app = express()
+var hbs = require('hbs')
 var session = require('express-session')
 var MongoClient = require('mongodb').MongoClient
 var url = 'mongodb://localhost:27017'
@@ -7,37 +8,44 @@ var url = 'mongodb://localhost:27017'
 const oneDay = 1000 * 60 * 60 * 24;
 app.use(session({
     secret: "thisismysecrctekeyfhrgfgrfrty84fwir767",
-    saveUninitialized:true,
+    saveUninitialized: true,
     cookie: { maxAge: oneDay },
-    resave: false 
+    resave: false
 }));
 
+hbs.registerHelper("increasedByOne", index => {
+    return ++index
+})
 
-app.set('view engine','hbs')
-app.use(express.urlencoded({extended:true}))
+hbs.registerHelper("isAdmin", role => {
+        return role === "Admin"
+})
 
-app.post('/login', async (req,res)=>{
+
+app.set('view engine', 'hbs')
+app.use(express.urlencoded({ extended: true }))
+
+app.post('/login', async (req, res) => {
     let name = req.body.txtName
     let pass = req.body.txtPass
 
-    let client= await MongoClient.connect(url);
+    let client = await MongoClient.connect(url);
     let dbo = client.db("ProductDB");
-    const user = await dbo.collection("users").findOne({'name' :name,'password': pass})
-    if(user !=null){
+    const user = await dbo.collection("users").findOne({ 'name': name, 'password': pass })
+    if (user != null) {
         console.log(`Name : ${user.name} Role: ${user.role}`)
-        var session=req.session
+        var session = req.session
         session.user = user
         res.redirect('/')
-    }else{
+    } else {
         res.redirect('/login')
     }
-
 })
-app.get('/login', (req,res)=>{
+app.get('/login', (req, res) => {
     res.render('login')
 })
 
-app.post('/register',async (req,res)=>{
+app.post('/register', async (req, res) => {
     let name = req.body.txtName
     let pass = req.body.txtPass
     let role = req.body.role
@@ -46,19 +54,19 @@ app.post('/register',async (req,res)=>{
         'password': pass,
         'role': role
     }
-    let client= await MongoClient.connect(url);
+    let client = await MongoClient.connect(url);
     let dbo = client.db("ProductDB");
     await dbo.collection("users").insertOne(user)
     res.redirect('/')
 })
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     const session = req.session
     const user = session.user
-    res.render('home',{'user': user})
+    res.render('home', { 'user': user })
 })
 
-app.get('/register',(req,res)=>{
+app.get('/register', (req, res) => {
     res.render('register')
 })
 
